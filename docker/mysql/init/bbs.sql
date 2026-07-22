0a0ca61c7cc41d4dfdf7775d49e45a1189c119d0
haruki26 <h826.a519@gmail.com>
2026-07-22 11:56:36 +0900

    chore: mysqlの設定ファイルを切り出しコンテナ起動時にデータベース作成が走るように

---
+++ docker/mysql/init/bbs.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS example_db;
+CREATE DATABASE example_db;
+USE example_db;
+
+DROP TABLE IF EXISTS bbs_entries;
+CREATE TABLE `bbs_entries` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `body` TEXT NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `image_filename` TEXT DEFAULT NULL
+);
